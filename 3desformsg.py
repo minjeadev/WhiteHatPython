2944536d25d338ef1275e225154606572a401605
minjeadev <minjea.dev@gmail.com>
2023-07-10 13:44:16 +0900

    3desformsg.py has been updated!!

---
+++ 3desformsg.py
@@ -0,0 +1,36 @@
+from Crypto.Cipher import DES3
+from Crypto.Hash import SHA256
+
+class MyDES():
+  def __init__(self, keytext, ivtext):
+    hash = SHA256.new()
+    hash.update(keytext.encode('utf-8'))
+    key = hash.digest()
+    self.key = key[:24]
+    hash.update(ivtext.encode('utf-8'))
+    iv = hash.digest()
+    self.iv = iv[:8]
+
+  def enc(self, plaintext):
+    des3 = DES3.new(self.key, DES3.MODE_CBC, self.iv)
+    encmsg = des3.encrypt(plaintext.encode())
+    return encmsg
+  
+  def dec(self, ciphertext):
+    des3 = DES3.new(self.key, DES3.MODE_CBC, self.iv)
+    decmsg = des3.decrypt(ciphertext)
+    return decmsg
+  
+def main():
+  keytext = 'samsjang'
+  ivtext = '1234'
+  msg = 'python3x'
+  myCipher = MyDES(keytext, ivtext)
+  ciphered = myCipher.enc(msg)
+  deciphered = myCipher.dec(ciphered)
+  print('ORIGINAL:\t%s' % msg)
+  print('CIPHERED:\t%s' % ciphered)
+  print('DECIPHERED:\t%s' % deciphered.decode())
+
+if __name__ == '__main__':
+  main()
